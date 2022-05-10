2990b42f7a93f9dd9037ba46a5317c74bb11778e
kishenv <72485496+kishenv@users.noreply.github.com>
2022-05-10 14:44:25 +0530

    Address review comments and refactor code.
    Accept and generate output as an array of integers of format [x,y,z] and refactor code.

--- LargestNumber.go
+++ LargestNumber.go
@@ -1,3 +1,8 @@
+
+///Problem statement : Generate the largest possible number from a given array of numbers.
+// Input : [9,93,24,6]
+// Output : [9,93,6,24]
+
 package main
 
 import (
@@ -7,40 +12,55 @@ import (
 )
 
 var (
-	numArray   []string
-	largestNum string
-	userInput  string
+	numArray            []string
+	largestNum          []string
+	userInput           string
+	containsNonZeroNums bool
+	inputLength         int
 )
 
 // largestNumGenerator
 // Function to generate the largest number from the inputs provided by user.
-func largestNumGenerator(numArray []string) string {
-	for i := 0; i < len(numArray); i++ {
-		for j := i + 1; j < len(numArray); j++ {
-			if numArray[i]+numArray[j] > numArray[j]+numArray[i] {
+func largestNumGenerator(numArray []string) {
+	containsNonZeroNums = false
+	inputLength = len(numArray)
+	for i := 0; i < inputLength; i++ {
+		for j := i + 1; j < inputLength; j++ {
+			if numArray[i]+numArray[j] < numArray[j]+numArray[i] {
 				numArray[i], numArray[j] = numArray[j], numArray[i]
 
 			}
 		}
 	}
 	for _, i := range numArray {
-		largestNum = i + largestNum
+		val, ok := strconv.Atoi(i)
+		if ok != nil {
+			fmt.Print("ERROR: Atoi conversion failed! Input contains non integer values/ bad input format")
+			return
+		}
+		if val != 0 {
+			containsNonZeroNums = true
+		}
+		largestNum = append(largestNum, i)
 	}
-	val, err := strconv.Atoi(largestNum)
-	if err != nil {
-		fmt.Print("ERROR: Atoi conversion failed! Input contains special-characters/alphabets")
-		return ""
-	}
-	if val == 0 {
-		return "0" // Corner case where the inputs provided is a series of 0s.
+	if !containsNonZeroNums {
+		fmt.Print([]string{"0"}) // Corner case where the input is a series of zeroes.
 	} else {
-		return largestNum
+		fmt.Print("[")
+		for ind, i := range largestNum {
+			if inputLength-1 == ind {
+				fmt.Print(i, "]")
+			} else {
+				fmt.Print(i, ",")
+			}
+		}
 	}
 }
 
 func main() {
-	fmt.Println("Enter a comma-separated list of numbers to generate largest number")
+	fmt.Println("Enter an array of numbers to generate the largest possible number. E.g. [3,4,5]")
 	fmt.Scanf("%s", &userInput)
+	userInput = userInput[1 : len(userInput)-1]
 	numArray = strings.Split(userInput, ",")
-	print(largestNumGenerator(numArray))
+	largestNumGenerator(numArray)
 }
