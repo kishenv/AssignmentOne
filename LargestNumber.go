c5c3f725e70295add2984cf9e62feef0b24b283f
kishenv <72485496+kishenv@users.noreply.github.com>
2022-05-11 19:56:34 +0530

    Fix : Address review comment.
    Changes to accept and return the result as an integer array.

--- LargestNumber.go
+++ LargestNumber.go
@@ -1,66 +1,64 @@
-
 ///Problem statement : Generate the largest possible number from a given array of numbers.
-// Input : [9,93,24,6]
-// Output : [9,93,6,24]
+// Input :  9 93 24 6
+// Output : [9 93 6 24]
 
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 	"strconv"
 	"strings"
 )
 
 var (
-	numArray            []string
-	largestNum          []string
-	userInput           string
+	numArray            []int
+	largestNum          []int
 	containsNonZeroNums bool
 	inputLength         int
 )
 
 // largestNumGenerator
-// Function to generate the largest number from the inputs provided by user.
-func largestNumGenerator(numArray []string) {
+// Function to generate the largest number from an array of integers.
+// Returns an array of integers arranged to generate the largest number
+func largestNumGenerator(numArray []int) []int {
 	containsNonZeroNums = false
 	inputLength = len(numArray)
 	for i := 0; i < inputLength; i++ {
 		for j := i + 1; j < inputLength; j++ {
-			if numArray[i]+numArray[j] < numArray[j]+numArray[i] {
+			toStri := strconv.Itoa(numArray[i])
+			toStrj := strconv.Itoa(numArray[j])
+			if toStri+toStrj < toStrj+toStri {
 				numArray[i], numArray[j] = numArray[j], numArray[i]
 
 			}
 		}
 	}
 	for _, i := range numArray {
-		val, ok := strconv.Atoi(i)
-		if ok != nil {
-			fmt.Print("ERROR: Atoi conversion failed! Input contains non integer values/ bad input format")
-			return
-		}
-		if val != 0 {
+		if i != 0 {
 			containsNonZeroNums = true
 		}
 		largestNum = append(largestNum, i)
 	}
 	if !containsNonZeroNums {
-		fmt.Print([]string{"0"}) // Corner case where the input is a series of zeroes.
+		return []int{0} // Corner case where the input is a series of zeroes.
 	} else {
-		fmt.Print("[")
-		for ind, i := range largestNum {
-			if inputLength-1 == ind {
-				fmt.Print(i, "]")
-			} else {
-				fmt.Print(i, ",")
-			}
-		}
+		return largestNum
 	}
 }
 
 func main() {
-	fmt.Println("Enter an array of numbers to generate the largest possible number. E.g. [3,4,5]")
-	fmt.Scanf("%s", &userInput)
-	userInput = userInput[1 : len(userInput)-1]
-	numArray = strings.Split(userInput, ",")
-	largestNumGenerator(numArray)
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	input := strings.Fields(scanner.Text())
+	for _, i := range input {
+		digit, err := strconv.Atoi(i)
+		if err != nil {
+			fmt.Print("Error: Non integer character present.")
+			return
+		}
+		numArray = append(numArray, digit)
+	}
+	fmt.Println(largestNumGenerator(numArray))
 }
